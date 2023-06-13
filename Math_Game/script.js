9f0287b2b8d4fb4ea461098512acdbc08a1e2768
gumusmuratgm@gmail.com <gumusmuratgm@gmail.com>
2023-06-13 08:49:55 +0300

    other projects added

---
+++ Math_Game/script.js
@@ -0,0 +1,11 @@
+let operators = ["+", "-", "*"]
+const startBtn = document.getElementById('start-btn');
+const question = document.getElementById('question');
+const controls = document.querySelector('controls-container');
+const result = document.getElementById('result');
+const submitBtn = document.getElementById('submit-btn');
+const errorMessage = document.getElementById('error-msg');;
+let answerValue;
+let operatorQuestion;
+
+const randomValue = (min, max) => Math.floor(Math.random()*(max-mi));

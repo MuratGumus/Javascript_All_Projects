5ee2008a11b01dfe853c2203921cc051e7bf7119
gumusmuratgm@gmail.com <gumusmuratgm@gmail.com>
2023-06-13 12:02:16 +0300

    app

--- Calculator/app.js
+++ Calculator/app.js
@@ -2,19 +2,80 @@ const previous = document.querySelector(".previous-display");
 const current = document.querySelector(".current-display");
 const button = document.querySelector(".buttons-container");
 // ! global alan
+let üstsatir = "";
 let altsatir = "";
 let operator = "";
+
 button.addEventListener("click", (e) => {
     if (e.target.classList.contains("num")) {
         sayiyiAl(e.target.textContent);
         yazdir();
     }
+    if (e.target.classList.contains("operator")) {
+        choose(e.target.textContent);
+        yazdir();
+    }
+    if (e.target.classList.contains("equal")) {
+        hesapla();
+        yazdir();
+    }
+    if (e.target.classList.contains("pm")) {
+        if (!altsatir) return;
+        altsatir *= -1;
+        yazdir();
+    }
+    if (e.target.classList.contains("percent")) {
+        if (!altsatir) return;{
+            altsatir = altsatir/100
+            yazdir()
+        }
+    }
+    if (e.target.classList.contains("pm")) {
+        yazdir();
+    }
 });
+
 const sayiyiAl = (num) => {
     altsatir += num;
-    console.log(altsatir);
+    //   console.log(altsatir);
 };
+
 const yazdir = () => {
-    current.textContent = altsatır;
-    previous.textContent = `${altsatır}${operator}`;
+    previous.textContent = `${altsatir}${operator}`;
+    current.textContent = altsatir;
+};
+
+const choose = (op) => {
+    operator = op;
+    üstsatir = altsatir;
+    altsatir = "";
+};
+
+const hesapla = () => {
+    let calculation = 0;
+    const sayı1 = Number(üstsatir);
+    const sayı2 = Number(altsatir);
+    console.log(sayı1);
+    console.log(sayı2);
+    switch (operator) {
+        case "+":
+            calculation = sayı1 + sayı2;
+            console.log(calculation);
+            break;
+        case "-":
+            calculation = sayı1 - sayı2;
+            break;
+        case "x":
+            calculation = sayı1 * sayı2;
+            break;
+        case "÷":
+            calculation = sayı1 / sayı2;
+            break;
+
+        default:
+            return;
+    }
+    altsatir = calculation;
+    üstsatir = "";
+    operator = "";
 };

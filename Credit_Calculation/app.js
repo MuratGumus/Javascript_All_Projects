a1d792f8fc99c033881d0e12b819857531ffaea4
gumusmuratgm@gmail.com <gumusmuratgm@gmail.com>
2023-07-03 09:13:53 +0300

    credit added

--- Credit_Calculation/app.js
+++ Credit_Calculation/app.js
@@ -5,46 +5,47 @@ const btn = document.querySelector(".btn");
 const form = document.querySelector("form");
 const sonuclar = document.querySelector(".sonuclar");
 
-let taksitTutari = 0;
+let taksitTutarı = 0;
 let faizOran = 0;
 let toplamTutar;
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (confirm("emin miziniz")) {
-        if (select.value === "Konut Kredisi") {
-            faizOran = 2.88;
-        } else if (select.value === "Ihtiyac Kredisi") {
-            faizOran = 1.99;
-        } else if (select.value === "Arac Kredisi") {
-            faizOran = 1.59;
-        }
-
-        const faiz = faizOran / 100; //! 100 -1200 -
-
-        taksitTutari =
-            (tutar.value * (faizOran * (1 + faizOran) ** vade.value) /
-            (((1 + faizOran) ** vade.value) - 1));
-        toplamTutar = taksitTutari * vade.value;
-        table();
-        //!  Kredi Tutarı * Faiz * (1+Faiz)**Taksit Sayısı /
-        // ! (1+Faiz)**Taksit Sayısı-1]
+  if (confirm("emin miziniz")) {
+    if (select.value === "Konut Kredisi") {
+      faizOran = 1.29;
+    } else if (select.value === "Ihtiyac Kredisi") {
+      faizOran = 1.99;
+    } else if (select.value === "Arac Kredisi") {
+      faizOran = 1.59;
     }
+
+    const faiz = faizOran / 100; //! 100 -1200 -
+
+    taksitTutarı =
+    (tutar.value * faiz * (1 + faiz) ** vade.value) /
+    ((1 + faiz) ** vade.value - 1);
+    toplamTutar = taksitTutarı * vade.value;
+    table()
+    //!  Kredi Tutarı * Faiz * (1+Faiz)**Taksit Sayısı /
+    // ! (1+Faiz)**Taksit Sayısı-1]
+  }
 });
 
-const table = () => {
-    sonuclar.innerHTML = `<table class="table table-sm">
+const table = ()=>{
+sonuclar.innerHTML = 
+`<table class="table table-sm">
 <thead>
-<th>İhtiyaç</th>
-<th>Vade</th>
-<th>Toplam Tutar:</th>
-<th>Taksit Tutari:</th>
+  <th>İhtiyaç</th>
+  <th>Vade</th>
+  <th>Toplam Tutar:</th>
+  <th>Taksit Tutarı:</th>
 </thead>
 <tbody>
 <td>${tutar.value}</td>
 <td>${vade.value}</td>
 <td>${toplamTutar.toFixed(2)}</td>
-<td>${taksitTutari.toFixed(2)}</td>
+<td>${taksitTutarı.toFixed(2)}</td>
 </tbody>
-</table> `;
-};
+</table> `
+}
